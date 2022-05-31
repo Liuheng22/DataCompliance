b2cb7831b9d2757496f2d072e48dcc0f1882a6cf
hs <liuheng19@126.com>
2022-05-31 19:16:33 +0800

    solve problem index,name mixture

--- router/upload/v1_upload/upload.go
+++ router/upload/v1_upload/upload.go
@@ -32,22 +32,25 @@ func HandleText(data *text.Test) *text.Testres {
 	res := &text.Testres{Data: make([]text.Rowres, 0)}
 
 	//处理每一行然后添加问题
+	index := 0
+	rowres := &text.Rowres{}
 	for _, row := range data.Data {
-		res.Data = append(res.Data, *HandleRowdata(&row))
+		rowres, index = HandleRowdata(&row, index)
+		res.Data = append(res.Data, *rowres)
 	}
 	return res
 }
 
 //处理Rowdata
-func HandleRowdata(row *text.Rowdata) *text.Rowres {
+func HandleRowdata(row *text.Rowdata, index int) (*text.Rowres, int) {
 	resrow := &text.Rowres{Key: row.Key, Name: row.Name, Age: row.Age, Phone: row.Phone, Address: row.Address, Id: row.Id, Problems: make([]text.Problem, 0)}
 	// 发现并且添加问题
 	// name问题
 	problems := make([]text.Problem, 0)
-	HandleName(row.Name, problems)
+	problems = append(problems, HandleName(row.Name, index+len(problems))...)
 
 	// phone问题
-	HandlePhone(row.Phone, problems)
+	problems = append(problems, HandlePhone(row.Phone, index+len(problems))...)
 
 	// address问题
 	HandleAddress(row.Address, problems)
@@ -57,29 +60,30 @@ func HandleRowdata(row *text.Rowdata) *text.Rowres {
 
 	// 将问题汇总
 	resrow.Problems = append(resrow.Problems, problems...)
-	return resrow
+	return resrow, index + len(resrow.Problems)
 }
 
 // 处理name问题
-func HandleName(name string, problems []text.Problem) {
+func HandleName(name string, index int) []text.Problem {
 	// 名字的问题
+	problems := make([]text.Problem, 0)
 	// 首先名字少于等于一个字，认为名字不完整
 	if len(name) <= 1 {
 		problems = append(problems, text.Problem{
-			ID:          len(problems),
+			ID:          index + len(problems),
 			Col:         "Name",
 			Seriousness: "caution",
 			Type:        "完整性",
 			Description: "缺少姓名信息",
 		})
-		return
+		return problems
 	}
 
 	// 名字的隐私性
 	count := strings.Count(name, "*")
 	// 有隐蔽，没问题
 	if count != 0 {
-		return
+		return problems
 	}
 	namerune := []rune(name)
 	// 有错构造新的name
@@ -87,17 +91,17 @@ func HandleName(name string, problems []text.Problem) {
 	newname[0] = namerune[0]
 	newname[1] = rune('*')
 	if len(namerune) > 2 {
-		newname = append(newname, namerune[len(newname)-1])
+		newname = append(newname, namerune[len(namerune)-1])
 	}
 	problems = append(problems, text.Problem{
-		ID:          len(problems),
+		ID:          index + len(problems),
 		Col:         "Name",
 		Seriousness: "risky",
 		Type:        "隐私性",
 		Description: "具体姓名需要隐去",
 		Fix:         string(newname),
 	})
-
+	return problems
 	// // 判断是否都是***
 	// flag := true
 	//
@@ -118,31 +122,33 @@ func HandleName(name string, problems []text.Problem) {
 }
 
 // 处理phone问题
-func HandlePhone(phone string, problems []text.Problem) {
+func HandlePhone(phone string, index int) []text.Problem {
 	// phone问题处理
+	problems := make([]text.Problem, 0)
 	// phone缺失
 	if len(phone) == 0 {
 		problems = append(problems, text.Problem{
-			ID:          len(problems),
+			ID:          index + len(problems),
 			Col:         "Phone",
 			Seriousness: "caution",
 			Type:        "完整性",
 			Description: "缺少电话信息",
 		})
 		//	电话位数不对，那么不需要考虑后续的东西了
-		return
+		index++
+		return problems
 	}
 	// phone的位数
 	if len(phone) != 11 {
 		problems = append(problems, text.Problem{
-			ID:          len(problems),
+			ID:          index + len(problems),
 			Col:         "Phone",
 			Seriousness: "critical",
 			Type:        "规范性",
 			Description: "phone格式与常规不符,且具体电话号码需隐去",
 		})
 		//	电话位数不对，那么不需要考虑后续的东西了
-		return
+		return problems
 	}
 
 	// 电话隐私性问题
@@ -152,13 +158,13 @@ func HandlePhone(phone string, problems []text.Problem) {
 	pos := strings.Index(phone, "****")
 	if cnt > 0 && pos == 3 {
 		// 隐私上没有错误
-		return
+		return problems
 	}
 	// 隐私上有问题
 	// 生成正确的phone
 	newphone := string(phone[0:3] + "****" + phone[7:])
 	problems = append(problems, text.Problem{
-		ID:          len(problems),
+		ID:          index + len(problems),
 		Col:         "Phone",
 		Seriousness: "critical",
 		Type:        "隐私性",
@@ -166,7 +172,7 @@ func HandlePhone(phone string, problems []text.Problem) {
 		Fix:         newphone,
 	})
 	//	电话位数不对，那么不需要考虑后续的东西了
-	return
+	return problems
 }
 
 // 处理address问题
